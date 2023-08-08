548c3ea1ba1be284a68a0e496e5f4e3ed530c03c
Lívia Souza <livia.terra@aluno.senai.br>
2023-08-08 14:32:56 -0300

    feat: create storePosts function

--- assets/js/script.posts.js
+++ assets/js/script.posts.js
@@ -1,3 +1,5 @@
+const posts = [];
+
 function savePost() {
     const title = document.getElementById("inputTitle").value;
     const category = document.getElementById("inputCategory").value;
@@ -5,9 +7,12 @@ function savePost() {
     const author = document.getElementById("inputAuthor").value;
     const date = document.getElementById("inputDate").value;
 
-    console.log(title, category, resume, author, date);
-
-    cleanFields();
+    if (title && category && resume && author && date) {
+        storePosts(title, category, resume, author, date);
+        cleanFields();
+    } else {
+        alert("Preencha todos os campos");
+    }
 }
 
 function cleanFields() {
@@ -16,4 +21,19 @@ function cleanFields() {
     const resume = document.getElementById("inputResume").value = "";
     const author = document.getElementById("inputAuthor").value = "";
     const date = document.getElementById("inputDate").value = "";
+}
+
+function storePosts(title, category, resume, author, date) {
+
+    const post = {
+        title,
+        category,
+        resume,
+        author,
+        date
+    };
+
+posts.push(post);
+
+console.log(posts);
 }
