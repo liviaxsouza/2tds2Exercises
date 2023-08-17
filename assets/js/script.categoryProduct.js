39baa5b280e6b34b0c6d4f9da69c9e340be0f6ae
Lívia Souza <livia.terra@aluno.senai.br>
2023-08-17 10:17:13 -0300

    feat: add class Product

--- assets/js/script.categoryProduct.js
+++ assets/js/script.categoryProduct.js
@@ -1,9 +1,19 @@
-// Uma categoria possui vários produtos 
+// Uma categoria possui vários produtos.
 
 class Category {
     constructor (id, name) {
         this.id = id;
         this.name = name;
         this.products = [];
+    }
+}
+
+// Produto pertence a uma categoria.
+class Products {
+    constructor(id, name, price, category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
     }
 }
